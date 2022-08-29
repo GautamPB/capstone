77c9e63ab035104d5c22d6653b9993daa9f2b14c
GautamPB <pbg2402@gmail.com>
2022-08-29 12:37:55 +0530

    Added: mask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from numpy import asarray
 
 image = cv2.imread('./Assets/Panel 1.jpg')
 
@@ -11,16 +12,31 @@ hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 blur = cv2.GaussianBlur(gray, (171,171), cv2.BORDER_DEFAULT)
 
-canny = cv2.Canny(hsv, 500, 500)
+canny = cv2.Canny(hsv, 450, 450)
+# canny = cv2.Canny(blur, 200, 200)
+
+percentage = 0
 
 ret, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
 
 cv2.imshow('Binary image', thresh)
 
-# lb = np.array([255, 228, 196])
-# hb = np.array([165, 42, 42])
+lb = np.array([0, 25, 10])
+hb = np.array([20, 155, 150])
 
-# mask = cv2.inRange(hsv, lb, hb)
+mask = cv2.inRange(hsv, lb, hb)
+
+segmented_img = cv2.bitwise_and(image, image, mask=mask)
+
+numpydata = asarray(mask)
+
+brown = np.count_nonzero(numpydata != 0)
+
+percentage = brown / numpydata.size * 100
+
+print(percentage, '%')
+
+cv2.imshow('Segmented Image', segmented_img)
 
 cv2.imshow('Original Image', image)
 
@@ -30,9 +46,9 @@ cv2.imshow('Original Image', image)
 
 # cv2.imshow('Canny edge', canny)
 
-# cv2.imshow('Mask', mask)
+cv2.imshow('Mask', mask)
 
-cv2.imshow('HSV', hsv)
+# cv2.imshow('HSV', hsv)
 
 cv2.waitKey(0)
 
